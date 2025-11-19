1ab6783a784e16b492fb29c9163e9b5ab04e7af0
HaroldOE <harold.ekempka@speedlinkng.com>
2025-11-19 06:08:03 +0100

    updated

---
+++ controllers/user.controller.js
@@ -0,0 +1,48 @@
+import { User } from "../models/";
+
+export const userController = {
+  create: async (req, res) => {
+    try {
+      const userId = await User.create(req.body);
+      res.json({ message: "User created", userId });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  },
+
+  findAll: async (req, res) => {
+    try {
+      const users = await User.findAll();
+      res.json(users);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  },
+
+  findById: async (req, res) => {
+    try {
+      const user = await User.findById(req.params.id);
+      res.json(user);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  },
+
+  update: async (req, res) => {
+    try {
+      const affected = await User.update(req.params.id, req.body);
+      res.json({ message: `${affected} record(s) updated` });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  },
+
+  delete: async (req, res) => {
+    try {
+      const affected = await User.delete(req.params.id);
+      res.json({ message: `${affected} record(s) deleted` });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  },
+};

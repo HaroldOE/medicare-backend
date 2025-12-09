8a43a22c09c0e29d867bd6374a5115b797520fce
HaroldOE <harold.ekempka@speedlinkng.com>
2025-12-09 03:42:57 +0100

    updated

--- models/db.js
+++ models/db.js
@@ -10,7 +10,7 @@ async function createConnection() {
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
-      // port: Number(process.env.DB_PORT) || 3306,
+      port: Number(process.env.DB_PORT) || 3306,
 
       // THESE ARE THE MISSING LINES THAT FIX ETIMEDOUT
       connectTimeout: 30000, // 30 seconds (default is only 10000)
